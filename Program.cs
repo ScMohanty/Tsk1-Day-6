0857f08dd7b4382abc50fdd10dc55f0e74628c0e
ScMohanty <92923028+ScMohanty@users.noreply.github.com>
2021-10-29 10:00:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticipentDetails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string particpentDetails;
+            Console.WriteLine("ENTER PARTICIPENT DETAILS");
+           particpentDetails = Console.ReadLine();
+          string [] s=  particpentDetails.Split(',');
+
+            foreach(string k in s)
+            {
+                Console.WriteLine(k);
+            }
+
+
+
+
+           
+        }
+    }
+}
